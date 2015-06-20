ae4e51f96c164dc49bbc8d4c656d9076e1733139
Patrick Tsai <yinghau76@gmail.com>
2015-06-20 08:30:21 +0800

    Initial check-in

---
+++ Hello/res/resources.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define HELLO "Hello"
---
+++ Hello/src/hello.cpp
@@ -0,0 +1,10 @@
+#include "hello.h"
+#include "resources.h"
+
+#include <iostream>
+
+using std::cout;
+
+void hello() {
+  cout << HELLO << "\n";
+}
